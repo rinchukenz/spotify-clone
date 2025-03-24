fa5932e13f867aad7f88e8f1bed973bcc11b9b76
rinchukenz <rinchu9999@gmail.com>
2025-03-24 23:20:26 +0530

    initial commit

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white lg:flex'>
+      <div className="bg-[#121212] "></div>
+    </div>
+  )
+}
+
+export default Sidebar
